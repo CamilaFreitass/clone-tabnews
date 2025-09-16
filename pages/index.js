19f19cb2208dd0d4bcaefb39469e2b7443f375e8
Camila Freitas <camilafreitas.economia@gmail.com>
2025-09-16 22:57:21 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Mor, vamos viajar? Se sim, já compra as passagens aí :)</h1>
+function Home() {
+  return <h1>Mor, vamos viajar? Se sim, já compra as passagens aí :)</h1>;
 }
 
 export default Home;
